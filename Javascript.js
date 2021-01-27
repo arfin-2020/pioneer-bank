8ab988563214262d2f3412cddaeee70924b29695
Arfin Chowdhury Arif <arfinchowdhuryarif7@gmail.com>
2021-01-27 11:09:00 +0600

    Added withdraw & balance

--- Javascript.js
+++ Javascript.js
@@ -11,15 +11,53 @@ loginbtn.addEventListener("click", function() {
 
 const depositbtn = document.getElementById("addDeposit");
 depositbtn.addEventListener("click", function() {
-    const depositAmount = document.getElementById("depositAmount").value;
-    // console.log(depositAmount);
-    const depositNumber = parseFloat(depositAmount);
+    //এখানে কমেন্ট এ যে 2 লাইন আছে সেগুলো হচ্ছে নরমান এবং এই 2 টা লাইনে একই কাজ বলে ফাংশনে করেছি।
 
-    const currentDeposit = document.getElementById("currentDeposit").innerText;
-    console.log(currentDeposit);
-    const currentDepositNumber = parseFloat(currentDeposit);
-    const totalDeposit = depositNumber + currentDepositNumber;
+    // const depositAmount = document.getElementById("depositAmount").value;
+    // // console.log(depositAmount);
+    // const depositNumber = parseFloat(depositAmount);
 
-    document.getElementById("currentDeposit").innerText = totalDeposit;
+    //এখানে কমেন্ট এ যে আটটা লাইন আছে সেগুলো হচ্ছে নরমান এবং এই ৮ টা লাইনে একই কাজ বলে ফাংশনে করেছি। 
 
+    // const currentDeposit = document.getElementById("currentDeposit").innerText;
+    // const currentDepositNumber = parseFloat(currentDeposit);
+    // const totalDeposit = depositNumber + currentDepositNumber;
+    // document.getElementById("currentDeposit").innerText = totalDeposit;
+
+    // const currentBalance = document.getElementById("currentBalance").innerText;
+    // const currentBalanceNumber = parseFloat(currentBalance);
+    // // console.log(currentBalanceNumber);
+    // const totalBalance = depositNumber + currentBalanceNumber;
+    // document.getElementById("currentBalance").innerText = totalBalance;
+
+    const depositNumber = getInputNumber("depositAmount"); //see in geInputNumber please for understand code
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+    document.getElementById("depositAmount").value = "";
 })
+
+//Withdraw button eventhandler
+const withdrawBtn = document.getElementById("addwithdraw"); //see in geInputNumber please for understand code
+
+withdrawBtn.addEventListener("click", function() {
+        // console.log("Withdraw Button is wroking");
+        //এখানে কমেন্ট এ যে 2 লাইন আছে সেগুলো হচ্ছে নরমান এবং এই 2 টা লাইনে একই কাজ বলে ফাংশনে করেছি।
+        // const withdrawAmount = document.getElementById("withdrawAmount").value;
+        // const withdrawNumber = parseFloat(withdrawAmount);
+        const withdrawNumber = getInputNumber("withdrawAmount");
+        // console.log(withdrawNumber);
+    })
+    // Withdraw Button Handler Function Start
+function getInputNumber(id) {
+    const Amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(Amount);
+    return amountNumber;
+}
+
+// Deposit Button Handler Function Start
+function updateSpanText(id, depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const total = depositNumber + currentNumber;
+    document.getElementById(id).innerText = total;
+}
